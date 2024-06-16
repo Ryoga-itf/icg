2bdfc47d3946592007ae09e4b646dde671756f69
Ryoga-exe <git@ryoga.dev>
2024-06-17 03:55:57 +0900

    feat: add slider

--- Modeler/include/controller.h
+++ Modeler/include/controller.h
@@ -16,14 +16,13 @@ extern int max_frame_count;
 // コントローラ識別用の列挙子
 enum CONTROLLERS {
     FRAME_CONTROLS, // フレームコントローラ（変更しないこと)
-    X_POSITION,     // オブジェクトのX座標
-    Y_POSITION,     // オブジェクトのY座標
-    Z_POSITION,     // オブジェクトのZ座標
-    ARM1_ANGLE,     // アーム1の角度
-    ARM2_ANGLE,     // アーム2の角度
-    ARM3_ANGLE,     // アーム3の角度
-    ARM4_ANGLE,     // アーム4の角度
-    NUM_CONTROLS,   // コントローラ数（必ず末尾に追加）
+    L_ARM_ANGLE,
+    R_ARM_ANGLE,
+    L_HEAR_ANGLE,
+    R_HEAR_ANGLE,
+    NECK_X_ANGLE,
+    NECK_Y_ANGLE,
+    NUM_CONTROLS, // コントローラ数（必ず末尾に追加）
 };
 
 // コントローラの設定
@@ -32,18 +31,12 @@ inline void SetController(ModelerControl *controls) {
     // フレーム番号の制御用スライダ
     controls[FRAME_CONTROLS] = ModelerControl("Frame Number", 0, (float)max_frame_count, 1, 0);
 
-    // オブジェクトのX座標用スライダ
-    controls[X_POSITION] = ModelerControl("X-Position", -3.0f, 3.0f, 0.1f, 0.0f);
-    // オブジェクトのY座標用スライダ
-    controls[Y_POSITION] = ModelerControl("Y-Position", -3.0f, 3.0f, 0.1f, 0.0f);
-    // オブジェクトのZ座標用スライダ
-    controls[Z_POSITION] = ModelerControl("Z-Position", -3.0f, 3.0f, 0.1f, 0.0f);
-
-    // アームの角度用スライダ
-    controls[ARM1_ANGLE] = ModelerControl("Arm1-Angle", -180.0f, 180.0f, 1.0f, 30.0f);
-    controls[ARM2_ANGLE] = ModelerControl("Arm2-Angle", -180.0f, 180.0f, 1.0f, 90.0f);
-    controls[ARM3_ANGLE] = ModelerControl("Arm3-Angle", -180.0f, 180.0f, 1.0f, -30.0f);
-    controls[ARM4_ANGLE] = ModelerControl("Arm4-Angle", -180.0f, 180.0f, 1.0f, 30.0f);
+    controls[L_ARM_ANGLE] = ModelerControl("Left Arm Angle", -180.0f, 180.0f, 1.0f, 120.0f);
+    controls[R_ARM_ANGLE] = ModelerControl("Right Arm Angle", -180.0f, 180.0f, 1.0f, -120.0f);
+    controls[L_HEAR_ANGLE] = ModelerControl("Left Hear Angle", -180.0f, 180.0f, 1.0f, 0.0f);
+    controls[R_HEAR_ANGLE] = ModelerControl("Right Hear Angle", -180.0f, 180.0f, 1.0f, 0.0f);
+    controls[NECK_X_ANGLE] = ModelerControl("Neck-X Angle", -90.0f, 90.0f, 1.0f, 0.0f);
+    controls[NECK_Y_ANGLE] = ModelerControl("Neck-Y Angle", -90.0f, 90.0f, 1.0f, 0.0f);
 }
 
 // __CONTROLLER_H__
