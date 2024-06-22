bd70d0c86cf890d8a0373a7234fe4aaacc172289
Ryoga-exe <git@ryoga.dev>
2024-06-23 00:01:51 +0900

    feat: add RayTracer project

---
+++ RayTracer/src/scene/ray.cpp
@@ -0,0 +1,9 @@
+#include "scene/ray.h"
+#include "scene/material.h"
+#include "scene/scene.h"
+
+const Material &
+isect::getMaterial() const
+{
+    return material ? *material : obj->getMaterial();
+}
